3faa104f2184ef0fc537d4d3229023349c435268
unknown <saelrodriguez2911@gmail.com>
2024-10-19 13:05:04 -0400

    modificacion de los tres archivos

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
-console.log("Hola mundo")
+document.write("Hola,mundo"  + "<br>" + "<br>")
+
+
+
+
+
+function mifuncion(){
+   for( let numeros = 0; numeros <= 20; numeros++ ){
+    if(numeros%2 === 0){
+        document.write("el numero " + numeros + " es par" + "<br>")
+    }else{
+        document.write("el numero " + numeros + " es impar " + "<br>")
+    }
+}
+}
